64c57254b1e32950f79c0c0be4f05681f6f2a5b8
feby <fabianofaccini@libero.it>
2022-11-23 12:24:12 +0100

    User admin interface filter update 23-11-2022

--- userpark/admin.py
+++ userpark/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from userpark.models import User as myUser
 from userpark.models import StaffAuthTable
 from django.contrib.auth.models import Group
@@ -7,6 +6,20 @@ from django.contrib.auth.models import Group
 # Register your models here.
 
 
-admin.site.register(myUser)
-admin.site.register(StaffAuthTable)
+@admin.register(myUser)
+class UserAdmin(admin.ModelAdmin):
+    exclude = ('staff_assigned_code', 'propic', 'password', 'groups', 'user_permissions', 'is_active')
+    list_filter = (
+        ('is_staff_member', admin.BooleanFieldListFilter),
+        ('date_joined', admin.DateFieldListFilter),
+    )
+
+
+@admin.register(StaffAuthTable)
+class StaffAuthTableAdmin(admin.ModelAdmin):
+    list_filter = (
+        ('is_used', admin.BooleanFieldListFilter),
+    )
+
+
 admin.site.unregister(Group)
